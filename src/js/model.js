bf638560a9adadc3cb03bd4e9c93fd7c99a3fa8e
leloufadel <leloufadel@gmail.com>
2024-04-22 13:48:29 +0000

    Create config.js file

--- src/js/model.js
+++ src/js/model.js
@@ -1,8 +1,10 @@
+import { API_URL } from "./config";
+
 export const state = {
     recipe: {},
 };
 export const loadRecipe = async function(id){
-  const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+  const res = await fetch(`${API_URL}/${id}`);
   const data = await res.json();
 
   if (!res.ok) {
